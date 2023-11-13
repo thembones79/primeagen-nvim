c6fe957e6060b98e5a6c6e233424e10b6c9e4932
thembones79 <michal.szumnarski@gmail.con>
2023-11-13 14:20:42 +0100

    changed default colors

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,5 +1,5 @@
 function ColorMyPencils(color)
-	color = color or "gruvbox"
+	color = color or "mine-pine"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
@@ -9,6 +9,6 @@ function ColorMyPencils(color)
 end
 
 if not vim.g.neovide then
-     ColorMyPencils("gruvbox")
+     ColorMyPencils("mine-pine")
 end
 
