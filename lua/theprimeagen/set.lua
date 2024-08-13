7e5b93f51a6672ae5046116e5735b458d4a55a5b
thembones79 <michal.szumnarski@gmail.com>
2024-08-13 20:49:00 +0200

    better remaps

--- lua/theprimeagen/set.lua
+++ lua/theprimeagen/set.lua
@@ -34,7 +34,7 @@ vim.opt.colorcolumn = "80"
 
 vim.cmd("set mouse=")
 
-vim.cmd("let g:neovide_transparency=0.9")
+vim.cmd("let g:neovide_transparency=0.95")
 -- vim.cmd("set guifont=BBT:h9")
 -- vim.cmd("set guifont=FantasqueSansMono\\ NF:h13")
 -- vim.cmd("set guifont=FixedsysExcelsiorIIIb\\ Nerd\\ Font:h12")
